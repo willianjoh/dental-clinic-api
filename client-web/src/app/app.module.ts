617d9d90db82f6b7596dff193621160e00379f80
willi <willian.silva2016@outlook.com>
2022-01-06 23:48:01 -0300

    template basico

--- client-web/src/app/app.module.ts
+++ client-web/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './template/home/home.component';
+import { PacienteModule } from './views/paciente/paciente.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateModule
+    ReactiveFormsModule,
+    FormsModule,
+    
+    PacienteModule,
+    TemplateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client-web/src/app/template/template.module.ts
+++ client-web/src/app/template/template.module.ts
@@ -1,21 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
   ]
 })
 export class TemplateModule { }
